5a9aa8a63659894d248ff8a53fa865e31f094899
James van de Ven <hello@jamesvandeven.com>
2025-12-02 21:00:19 +0800

    Day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,17 +1,35 @@
 class Day02 {
-    companion object {
-        private fun part1(input: List<String>): Int = input.size
-
-        private fun part2(input: List<String>): Int = input.size
-
-        fun solve() {
-            check(part1(listOf("test_input")) == 1)
-            check(part2(listOf("test_input")) == 1)
-
-            val input = readInput("Day02")
-            part1(input).println()
-            part2(input).println()
+  companion object {
+    private fun part1(input: String) =
+        input.split(",").sumOf {
+          """(\d+)-(\d+)"""
+              .toRegex()
+              .matchEntire(it)
+              ?.destructured
+              ?.let { (a, b) -> a.toLong()..b.toLong() }
+              ?.filter { """(\d+)\1""".toRegex().matches(it.toString()) }
+              ?.sum() ?: throw Error()
         }
-    }
-}
 
+    private fun part2(input: String) =
+        input.split(",").sumOf {
+          """(\d+)-(\d+)"""
+              .toRegex()
+              .matchEntire(it)
+              ?.destructured
+              ?.let { (a, b) -> a.toLong()..b.toLong() }
+              ?.filter { """(\d+)\1+""".toRegex().matches(it.toString()) }
+              ?.sum() ?: throw Error()
+        }
+
+    fun solve() {
+      val testInput = readInput("Day02_Test").first()
+      check(part1(testInput) == 1227775554.toLong())
+      check(part2(testInput) == 4174379265.toLong())
+
+      val input = readInput("Day02").first()
+      part1(input).println()
+      part2(input).println()
+    }
+  }
+}
