011d0254872ec2441be03eab4f71e5253c5e4462
James van de Ven <hello@jamesvandeven.com>
2025-12-02 19:22:06 +0800

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,36 @@
+import kotlin.math.abs
+
 class Day01 {
-    companion object {
-        private fun part1(input: List<String>): Int = input.size
-
-        private fun part2(input: List<String>): Int = input.size
-
-        fun solve() {
-            check(part1(listOf("test_input")) == 1)
-            check(part2(listOf("test_input")) == 1)
-
-            val input = readInput("Day01")
-            part1(input).println()
-            part2(input).println()
-        }
+  companion object {
+    private val parseMoves = { line: String ->
+      """([L|R])(\d+)""".toRegex().matchEntire(line)?.destructured?.let { (direction, distance) ->
+        if (direction == "L") -distance.toInt() else distance.toInt()
+      } ?: throw Error("Bad input")
     }
+
+    private fun part1(input: List<String>): Int =
+        input.map(parseMoves).scan(50, Int::plus).count { it % 100 == 0 }
+
+    private fun part2(input: List<String>) =
+        input
+            .map(parseMoves)
+            .scan(50 to 0) { (lastLocation, _), move -> (lastLocation + move).mod(100) to move }
+            .zipWithNext { (lastLocation, _), (_, nextMove) -> lastLocation to nextMove }
+            .sumOf { (location, move) ->
+              // Revolutions in the move
+              abs(move) / 100 +
+                  // Whether the move crosses the threshold (aside from full revolutions)
+                  if (location != 0 && location + (move % 100) !in 1..99) 1 else 0
+            }
+
+    fun solve() {
+      val testInput = readInput("Day01_Test")
+      check(part1(testInput) == 3)
+      check(part2(testInput) == 6)
+
+      val input = readInput("Day01")
+      part1(input).println()
+      part2(input).println()
+    }
+  }
 }
