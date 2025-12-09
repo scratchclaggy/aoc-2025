4cb14e281cb8cbe69a47dd67d962c2c8f164a869
James van de Ven <hello@jamesvandeven.com>
2025-12-09 08:23:00 +0800

    Day 07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,17 +1,55 @@
 class Day07 {
-    companion object {
-        private fun part1(input: List<String>): Int = input.size
+  companion object {
+    private fun part1(input: List<String>) =
+        input
+            .drop(1)
+            .scan(input.first().toList()) { prev, next ->
+              val beams = prev.mapIndexed { i, c -> if (c == 'S') i else null }.filterNotNull()
+              val activeSplitters =
+                  next
+                      .mapIndexed { i, c -> if (c == '^' && i in beams) i else null }
+                      .filterNotNull()
+              next.mapIndexed { i, _ ->
+                when {
+                  i + 1 in activeSplitters || i - 1 in activeSplitters -> 'S'
+                  i in activeSplitters -> '^'
+                  i in beams -> 'S'
+                  else -> '.'
+                }
+              }
+            }
+            .flatten()
+            .count { it == '^' }
 
-        private fun part2(input: List<String>): Int = input.size
+    private fun part2(input: List<String>) =
+        input
+            .drop(1)
+            .fold(input.first().map { if (it == 'S') 1.toLong() else 0 }) { prev, next ->
+              val activeSplitters =
+                  next
+                      .mapIndexed { i, c -> if (c == '^' && prev[i] > 0) i else null }
+                      .filterNotNull()
+              next.mapIndexed { i, _ ->
+                when {
+                  i - 1 in activeSplitters && i + 1 in activeSplitters ->
+                      prev[i - 1] + prev[i] + prev[i + 1]
+                  i - 1 in activeSplitters -> prev[i - 1] + prev[i]
+                  i + 1 in activeSplitters -> prev[i + 1] + prev[i]
+                  i in activeSplitters -> 0
+                  else -> prev[i]
+                }
+              }
+            }
+            .sum()
 
-        fun solve() {
-            check(part1(listOf("test_input")) == 1)
-            check(part2(listOf("test_input")) == 1)
+    fun solve() {
+      val testInput = readInput("Day07_Test")
+      check(part1(testInput) == 21)
+      check(part2(testInput) == 40.toLong())
 
-            val input = readInput("Day07")
-            part1(input).println()
-            part2(input).println()
-        }
+      val input = readInput("Day07")
+      part1(input).println()
+      part2(input).println()
     }
+  }
 }
-
