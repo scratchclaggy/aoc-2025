14ef25ac842c67f524c2f2b333ad302319e33199
James van de Ven <hello@jamesvandeven.com>
2025-12-21 20:37:38 +0800

    Day 12

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,17 +1,23 @@
 class Day12 {
-    companion object {
-        private fun part1(input: List<String>): Int = input.size
+  companion object {
+    private fun part1(input: List<String>) =
+        input
+            .filter { it.contains("x") }
+            .let {
+              it.map {
+                it.split(": ").let {
+                  it.first().split("x").map { it.toInt() }.reduce { acc, n -> acc * n } to
+                      it.last().split(" ").map { it.toInt() }.sum() * 7
+                }
+              }
+            }
+            .count { (bins, boxes) -> bins >= boxes }
 
-        private fun part2(input: List<String>): Int = input.size
-
-        fun solve() {
-            check(part1(listOf("test_input")) == 1)
-            check(part2(listOf("test_input")) == 1)
-
-            val input = readInput("Day12")
-            part1(input).println()
-            part2(input).println()
-        }
+    fun solve() {
+      // Aproximation doesn't work with test input ¯\_(ツ)_/¯
+      //
+      val input = readInput("Day12")
+      part1(input).println()
     }
+  }
 }
-
